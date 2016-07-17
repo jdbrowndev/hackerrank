e037b0b7aa7ed26ece3e13dc276caa38fd91a682
Jordan Brown <emagdne@gmail.com>
2016-07-17 00:20:57 -0400

    Add solution to merging communities problem.

---
+++ data-structures/disjoint-sets/merging-communities/OldDictionarySolution.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// My original attempt at the problem using a Dictionary of HashSets.
+/// </summary>
+public class OldDictionarySolution
+{
+    private static Dictionary<int, HashSet<int>> Communities;
+
+    public static void RunOldDictionarySolution()
+    {
+        string[] args = Console.ReadLine().Split(' ');
+        int n = Int32.Parse(args[0]);
+        int q = Int32.Parse(args[1]);
+
+        Communities = new Dictionary<int, HashSet<int>>(n);
+
+        for (int i = 1; i <= n; i++)
+        {
+            var community = new HashSet<int>();
+            community.Add(i);
+            Communities[i] = community;
+        }
+
+        for (int i = 1; i <= q; i++)
+        {
+            string[] query = Console.ReadLine().Split(' ');
+
+            if (query[0] == "Q")
+            {
+                Console.WriteLine(QueryCommunity(Int32.Parse(query[1])));
+            }
+            else if (query[0] == "M")
+            {
+                MergeCommunities(Int32.Parse(query[1]), Int32.Parse(query[2]));
+            }
+        }
+    }
+
+    private static int QueryCommunity(int person)
+    {
+        return Communities[person].Count;
+    }
+
+    private static bool MergeCommunities(int personOne, int personTwo)
+    {
+        var communityOne = Communities[personOne];
+        var communityTwo = Communities[personTwo];
+
+        if (communityOne != null && communityTwo != null)
+        {
+            communityOne.UnionWith(communityTwo);
+            foreach (int person in communityTwo)
+            {
+                Communities[person] = communityOne;
+            }
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ data-structures/disjoint-sets/merging-communities/Solution.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+/// <summary>
+/// <para>Author: Jordan Brown</para>
+/// <para>Date: 7-17-16</para>
+/// <para>
+/// Solves the merging communities problem via a DisjointSet class.
+/// </para>
+/// <para>
+/// Link to problem: https://www.hackerrank.com/challenges/merging-communities
+/// </para>
+/// </summary>
+public class Solution
+{
+    public static void Main()
+    {
+        string[] args = Console.ReadLine().Split(' ');
+        int n = Int32.Parse(args[0]);
+        int q = Int32.Parse(args[1]);
+
+        DisjointSet set = new DisjointSet(n);
+
+        for (int i = 1; i <= q; i++)
+        {
+            string[] query = Console.ReadLine().Split(' ');
+
+            if (query[0] == "Q")
+            {
+                Console.WriteLine(set.GetSetSize(Int32.Parse(query[1])));
+            }
+            else if (query[0] == "M")
+            {
+                set.MergeSets(Int32.Parse(query[1]), Int32.Parse(query[2]));
+            }
+        }
+    }
+}
+
+public class DisjointSet
+{
+    private readonly int[] _sets;
+    private readonly int[] _ranks;
+
+    public DisjointSet(int size)
+    {
+        _sets = new int[size + 1];
+        _ranks = new int[size + 1];
+
+        for (int i = 1; i < size + 1; i++)
+        {
+            _sets[i] = i;
+            _ranks[i] = 1;
+        }
+    }
+
+    public int GetSetSize(int element)
+    {
+        int tail = FindTail(element);
+        return _ranks[tail];
+    }
+
+    public void MergeSets(int elementOne, int elementTwo)
+    {
+        int tailOne = FindTail(elementOne), tailTwo = FindTail(elementTwo);
+
+        if (tailOne != tailTwo)
+        {
+            _ranks[tailTwo] += _ranks[tailOne];
+            _sets[tailOne] = tailTwo;
+        }
+    }
+
+    private int FindTail(int element)
+    {
+        if (_sets[element] == element)
+        {
+            return element;
+        }
+
+        int tail = FindTail(_sets[element]);
+        _sets[element] = tail; // Optimizes path to tail in the future.
+        return tail;
+    }
+}
