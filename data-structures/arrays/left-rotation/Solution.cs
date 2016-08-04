c6943c0bbe616adf5258f65d2a76e9489ce1e6ee
Jordan Brown <emagdne@gmail.com>
2016-08-03 21:37:11 -0400

    Add left rotation/sparse arrays problems.

---
+++ data-structures/arrays/left-rotation/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/// <summary>
+/// <para>Author: Jordan Brown</para>
+/// <para>Date: 8-3-16</para>
+/// <para>
+/// Solves the array left rotation problem.
+/// </para>
+/// <para>
+/// Link to problem: https://www.hackerrank.com/challenges/array-left-rotation
+/// </para>
+/// </summary>
+public class Solution
+{
+    public static void Main()
+    {
+        // Read d (don't need n).
+        string[] args = Console.ReadLine().Split(' ');
+        int d = Convert.ToInt32(args[1]);
+
+        // Print state of array after d rotations.
+        string[] arr = Console.ReadLine().Split(' ');
+        for (int i = d; i < arr.Length; i++)
+        {
+            Console.Write($"{arr[i]} ");
+        }
+        for (int i = 0; i < d; i++)
+        {
+            Console.Write($"{arr[i]} ");
+        }
+    }
+}
---
+++ data-structures/arrays/sparse-arrays/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// <para>Author: Jordan Brown</para>
+/// <para>Date: 8-3-16</para>
+/// <para>
+/// Uses a dictionary to solve the sparse arrays problem.
+/// </para>
+/// <para>
+/// Link to problem: https://www.hackerrank.com/challenges/sparse-arrays
+/// </para>
+/// </summary>
+public class Solution
+{
+    private static Dictionary<string, int> _occurrences = new Dictionary<string, int>();
+
+    public static void Main()
+    {
+        // Map strings to number of occurrences.
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            string str = Console.ReadLine();
+            if (_occurrences.ContainsKey(str))
+            {
+                _occurrences[str]++;
+            }
+            else
+            {
+                _occurrences[str] = 1;
+            }
+        }
+
+        // Respond to queries.
+        int q = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i <= q; i++)
+        {
+            string query = Console.ReadLine();
+            Console.WriteLine(_occurrences.ContainsKey(query) ? _occurrences[query] : 0);
+        }
+    }
+}
